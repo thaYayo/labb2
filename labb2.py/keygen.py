9635213e7a1b9d8cd73edfbc03f0c80c92cd0165
unknown <lionel_lopcas96@hotmail.com>
2024-10-19 15:08:39 +0200

    fixed password salt encryption/decryption

--- labb2.py/keygen.py
+++ labb2.py/keygen.py
@@ -6,10 +6,10 @@ from cryptography.fernet import Fernet
 def key_generator(output=None):
     key = Fernet.generate_key()
     if output:
-        with open(output, "wb") as f:
+        with open(f"{output}.key", "wb") as f:
             f.write(key)
-            print(f"Nyckeln har sparats i {output}")
+            print(f"Nyckeln har sparats i {output}.key")
     else:
         with open("crypto.key", "wb") as key_file:
             key_file.write(key)
-            print(f"Nyckeln har sparats i {os.getcwd()}")
+            print(f"Nyckeln har sparats i {os.getcwd()}\\crypto.key")
