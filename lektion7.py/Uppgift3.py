31af365c5dd465e5b10a1bd303a5b3a3c1eba16e
thaYayo <153025998+thaYayo@users.noreply.github.com>
2024-10-19 14:50:11 +0200

    Delete lektion7.py directory

--- lektion7.py/Uppgift3.py
+++
@@ -1,7 +0,0 @@
-# Uppgift 3
-# Testa att använda breakpoint() / pdb.set_trace() i något lite längre script som du skrivit och
-# navigera dig fram i programflödet.
-# Testa att göra samma sak med din editor/IDE
-# Kör pylint på samma script och rätta till eventuella problem.
-
-
